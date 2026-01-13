27b433f3ffdf9a91d60f7e003e77070dc0c5d683
Jashwanth Reddy <jashwanthreddy312@gmail.com>
2026-01-13 17:49:40 +0530

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "project2-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_D2lds_v6"
+  size                = "Standard_D2s_v3"
   admin_username      = var.vm_user
 
   network_interface_ids = [
@@ -52,6 +52,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
 
 
 
