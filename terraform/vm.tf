d6c4e54c34bf41dacc3ad4ca3cef39adfacbfede
Jashwanth Reddy <jashwanthreddy312@gmail.com>
2026-01-13 16:35:22 +0530

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku = "standard"
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -51,3 +52,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
